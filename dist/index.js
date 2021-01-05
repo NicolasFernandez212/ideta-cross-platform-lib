9e5627ac043d881dd09b78d1761ace680d4e646b
nix_Ideta <nicolas@ideta.io>
2021-01-05 18:04:10 +0100

    Created test interface to export

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bot_entity_1 = require("./common/bot/entities/bot.entity");
+exports.CrossBotInterface = bot_entity_1.CrossBotInterface;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { CrossBotInterface } from "./common/bot/entities/bot.entity";
