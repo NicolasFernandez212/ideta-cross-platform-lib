cc0921d16c081e01e0041267dd4ce6c6364e2232
nix_Ideta <nicolas@ideta.io>
2021-01-05 20:12:09 +0100

    Updated package structure

---
+++ lib/common/billing/billing.d.ts
@@ -0,0 +1,4 @@
+import * as constants from "./constants";
+import * as entities from "./entities";
+import * as types from "./types";
+export { constants, entities, types };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-export {
-  AddonsPriceConvention,
-  PlanNameConvention,
-  Plans,
-} from "./common/billing/constants";
-export {
-  BillingPlan,
-  BotBillingAddons,
-  BotBillingOptions,
-} from "./common/billing/entities";
-export { PlanId, PlanName } from "./common/billing/types";
+import * as billing from "./common/billing/billing";
+
+export { billing };
