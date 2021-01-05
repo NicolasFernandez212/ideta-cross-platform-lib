51ddb0d4012eab285e732528de5e400ac38c4267
nix_Ideta <nicolas@ideta.io>
2021-01-05 17:53:15 +0100

    Updated config files

---
+++ dist/bot.entity.d.ts
@@ -0,0 +1,4 @@
+export declare class CrossBotInterface {
+    id: string;
+    name: string;
+}
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function test() {
-    console.log('Hello word !');
-}
-exports.test = test;
